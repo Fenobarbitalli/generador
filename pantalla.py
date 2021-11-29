45f61f7a5e40ae3385e0f8be95f4e4dba077ba56
Ricardo Alfonso Rojas Aranguren <87102043+Fenobarbitalli@users.noreply.github.com>
2021-11-29 09:20:57 -0500

    Update pantalla.py

--- pantalla.py
+++ pantalla.py
@@ -1,21 +1,21 @@
-
-import numpy as np
+import numpy as arreglo
 from PIL import Image, ImageTk
-#import cv2
 
-RESOLUCION = 512
-NUMERO_DATOS = 1024
+datos = 480
+forma = 240
+
 
 def funcion_a_imagen(color):
     
     
-    frame = np.zeros((RESOLUCION, NUMERO_DATOS,3),dtype=np.uint8)
-    for i in range(NUMERO_DATOS):
-        frame[RESOLUCION-i%RESOLUCION-1,i,:]=np.array(color)
+    frame = arreglo.zeros((forma, datos,3),'C' ,arreglo.uint8)
+
+    for i in range(datos):
+        frame[forma-i%forma-1,i,:]=arreglo.array(color)
 
     print(frame.shape,frame.dtype)
     im = Image.fromarray(frame)
-    img = ImageTk.PhotoImage(image=im)
+    imagen = ImageTk.PhotoImage(image=im)
 
  
-    return img
+    return imagen
